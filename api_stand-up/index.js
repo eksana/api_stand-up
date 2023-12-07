9a0792d1bfd666174195375ee9d7de7109c38530
Oksana Vainrub <ser.vajnrub@yandex.ru>
2023-12-07 21:34:20 +0300

    remove modules

--- api_stand-up/index.js
+++ api_stand-up/index.js
@@ -3,42 +3,48 @@ import fs from "node:fs/promises"
 
 const PORT = 8080;
 
-const data = await fs.readFile('package.json', 'utf-8');
-console.log(data);
+const startServer = async () => {
+
+    const data = await fs.readFile('package.json', 'utf-8');
+    console.log(data);
+
+    //const http = require("node:http");
+
+    //console.log(http);
+    http.createServer(async (req, res) => {
+        if (req.method === "GET" && req.url === "/comedians") {
+
+            try {
+                const data = await fs.readFile('comedians.json', 'utf-8')
+                res.writeHead(200, {
+                    //"Content-Type": "text/plain; charset=utf-8",
+                    "Content-Type": "text/json; charset=utf-8",
+                    "Access-control-Allow-Origin": "*",
+                });
+                //res.end("<h1>hello, привет</h1>");
+                res.end(data);
+            } catch (error) {
+                res.writeHead(500, {
+                    "Content-Type": "text/plain; charset=utf-8",
+                });
+                res.end(`Ошибка сервера: ${error}`)
+            }
 
 
-//const http = require("node:http");
-
-//console.log(http);
-http.createServer(async (req, res) => {
-    if (req.method === "GET" && req.url === "/comedians") {
-
-        try {
-            const data = await fs.readFile('comedians.json', 'utf-8')
-            res.writeHead(200, {
-                //"Content-Type": "text/plain; charset=utf-8",
-                "Content-Type": "text/json; charset=utf-8",
-                "Access-control-Allow-Origin": "*",
-            });
-            //res.end("<h1>hello, привет</h1>");
-            res.end(data);
-        } catch (error) {
-            res.writeHead(500, {
+        } else {
+            res.writeHead(404, {
                 "Content-Type": "text/plain; charset=utf-8",
             });
-            res.end(`Ошибка сервера: ${error}`)
+            res.end("не найдено");
         }
 
+    })
+        .listen(PORT);
 
-    } else {
-        res.writeHead(404, {
-            "Content-Type": "text/plain; charset=utf-8",
-        });
-        res.end("не найдено");
-    }
+    console.log(`Серевер запущен на http://localhost:${PORT}`);
 
-})
-    .listen(PORT);
+}
+startServer();
 
-console.log(`Серевер запущен на http://localhost:${PORT}`);
+
 
